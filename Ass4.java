7a9751409d2fc2e4e7d4bac368db977302b78ee1
abdirashid-bx <cabdirashiidbashiirxasan@gmail.com>
2025-10-19 14:15:22 +0300

    Update Ass4.java

--- Ass4.java
+++ Ass4.java
@@ -4,8 +4,8 @@ import java.util.EmptyStackException;
 public class Ass4 {
     public static void main(String[] args) {
         Arreystackimp<String> s1 = new Arreystackimp<>(3);
-        s1.add("batman");
-        s1.add("baba");
+        s1.add("BB");
+        s1.add("AA");
         s1.add("hh");
         s1.add("ee");
         s1.remove();
